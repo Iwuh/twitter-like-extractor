0fa57430556419e7eb9f4e7caa87936b26ba6438
Matthew Faigan <matthew.faigan@gmail.com>
2022-08-03 17:10:15 -0400

    Create project, basic file reading

---
+++ TwitterLikeExtractor/TwitterLikeExtractor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace TwitterLikeExtractor;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: TwitterLikeExtractor.exe filename");
+            return;
+        }
+
+        try
+        {
+            using JsonDocument har = JsonDocument.Parse(File.ReadAllText(args[0]));
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine($"Could not open file '{ex.FileName}'.");
+        }
+        catch (JsonException)
+        {
+            Console.WriteLine($"Could not deserialize the HAR file.");
+        }
+    }
+}
